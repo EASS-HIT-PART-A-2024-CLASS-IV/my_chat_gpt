37191e64f9c0782455f28e979fa70abafb0b7c9f
George Khananaev <george.khananaev@gmail.com>
2024-03-20 00:41:06 +0200

    added particles and did some design changes such as background

--- nextjs/src/pages/login.js
+++ nextjs/src/pages/login.js
@@ -4,12 +4,12 @@ import LoginBox from "@/sections/auth/login-box";
 
 export default function Login() {
     return (
-        <>
+        <div>
             <Grid container justifyContent="center" alignItems="center">
                 <Grid item xs={10} sm={8} md={6} lg={4}>
                     <LoginBox/>
                 </Grid>
             </Grid>
-        </>
+        </div>
     );
 }
