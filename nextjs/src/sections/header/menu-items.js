31b2cc675fd843f46f4a3f44aa80421b0866347c
George Khananaev <george.khananaev@gmail.com>
2024-03-19 23:31:23 +0200

    renamed home to README, default page is dashboard, added some dummy dashboard too.

--- nextjs/src/sections/header/menu-items.js
+++ nextjs/src/sections/header/menu-items.js
@@ -1,16 +1,16 @@
-import LogoutButton from "@/sections/auth/logout";
 import DashboardIcon from '@mui/icons-material/Dashboard';
 import AccountBoxIcon from '@mui/icons-material/AccountBox';
-import HomeIcon from '@mui/icons-material/Home';
+// import HomeIcon from '@mui/icons-material/Home';
 import LoginIcon from '@mui/icons-material/Login';
 import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 const iconStyle = {height: 16, width: 16, mb: "-3px", mr: 0.5}
 export const menuItems = [
     {
-        icon: <HomeIcon sx={iconStyle}/>,
-        label: "Home",
-        tooltip: "Home Page",
+        icon: <GitHubIcon sx={iconStyle}/>,
+        label: "README",
+        tooltip: "README.md Documentations",
         path: "/",
         authenticated: null
     },
